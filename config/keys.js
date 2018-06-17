6acc2b851753973144281d7362ab2dfe6c9ab631
shanmugam <shanmugam.vairavan@walgreens.com>
2018-06-17 14:10:30 -0500

    New changes

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '175674052300-murtm3676mjsifdv05av3l3do9r2osa3.apps.googleusercontent.com',
+            clientSecret: 'ZpXyCZ_duyUVlO-wT1TzK5uU'
+        },
+        mongodb: {
+            dbURI: 'mongodb://admin:Password123@ds159400.mlab.com:59400/idaas'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
